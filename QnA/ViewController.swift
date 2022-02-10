15d57c9f50ebde2921186fd2f6af6d41b1c128fc
AndresGutierrez <c0dingw.dre@gmail.com>
2022-02-10 10:50:49 -0800

    Checkpoint 1/ put together details for quiz page. set some constraints to get bal going

--- QnA/ViewController.swift
+++ QnA/ViewController.swift
@@ -9,11 +9,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var scoreLabel: UILabel!
+    var qLabel: UILabel!
+    var questionLabel: UILabel!
+    var questionContainer: UIView!
+    var answersContainerView: UIView!
+    var progressBar: UIProgressView!
+    var nextScreen: UIButton!
+    
+    
+    var score   = 0
+    var questionsRemaining  = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor    = .systemBlue
+        configureLabels()
+        configureConstraints()
     }
 
+    
+    func configureLabels() {
+        scoreLabel                  = UILabel()
+        scoreLabel.font             = UIFont.systemFont(ofSize: 18)
+        scoreLabel.textAlignment    = .right
+        scoreLabel.text             = "Score: \(score)"
+        scoreLabel.translatesAutoresizingMaskIntoConstraints        = false
+        view.addSubview(scoreLabel)
+        
+        questionContainer           = UIView()
+        questionContainer.backgroundColor   = .systemGray
+        questionContainer.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(questionContainer)
+        
+        qLabel                      = UILabel()
+        qLabel.textAlignment        = .center
+        qLabel.text                 = "Q"
+        qLabel.font                 = UIFont.systemFont(ofSize: 33)
+        qLabel.translatesAutoresizingMaskIntoConstraints            = false
+        questionContainer.addSubview(qLabel)
+    }
 
+    
+    
+    func configureConstraints() {
+        
+        NSLayoutConstraint.activate([
+            scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 20),
+            scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: -5),
+            
+            questionContainer.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor, constant: 30),
+            questionContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            questionContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            questionContainer.heightAnchor.constraint(equalToConstant: 134),
+            
+            
+        ])
+    }
 }
 

01989e4174625a8051acddb7883df58cd6ff6e56
AndresGutierrez <c0dingw.dre@gmail.com>
2022-02-09 17:44:30 -0800

    Initial Commit

---
+++ QnA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QnA
+//
+//  Created by Andres Gutierrez on 2/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
